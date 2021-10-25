3b205dc86ff99b286f5a34f9870ea0f213eda77c
KUrvi <korgaonkarurraj19ce@student.mes.ac.in>
2021-10-25 21:56:59 +0530

    Added Sample.py

---
+++ Sample.py
@@ -0,0 +1,16 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+num1=1.7
+num2=7.8
+
+sum=num1+num2
+
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
+The sum of 1.7 and 7.8 is 9.5
+
+print("Hello TE Comps)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("Hello TE Comps")
+      
+Hello TE Comps
